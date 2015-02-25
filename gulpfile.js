e7af58e14bb961c7edbf40f51c8999342901d17e
JD Isaacks <jd@jisaacks.com>
2015-02-25 09:26:42 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp        = require("gulp");
+var less        = require("gulp-less");
+
+var lessPath    = "./less/**/*.less";
+
+gulp.task("less", function() {
+
+  gulp.src(lessPath)
+
+    .pipe(less())
+
+    .pipe(gulp.dest("./css"));
+
+});
+
+gulp.task("watch", function() {
+  gulp.watch(lessPath, ["less"]);
+});
